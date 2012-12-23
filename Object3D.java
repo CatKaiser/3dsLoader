2fcec39898711a4672768597c2a28545cd0d9e63
CatKaiser <catmaster321@yahoo.com>
2012-12-23 06:17:22 -0800

    Create Object3D.java

---
+++ Object3D.java
@@ -0,0 +1,12 @@
+public class Object3D {
+  
+	public String name;
+	public int vertCount;
+	public int indCount;
+	public ArrayList<FaceMat> faceMats;
+	public int glVertices;
+	public int glIndices;
+	public float[] vertexBuffer;
+	public float[] trMatrix;
+
+}
