5387985dbb036a1f532026d09741823aad44cff7
CatKaiser <catmaster321@yahoo.com>
2012-12-23 06:17:06 -0800

    Create Material3D.java

---
+++ Material3D.java
@@ -0,0 +1,12 @@
+public class Material3D {
+  public String name;
+	public float[] ambient;
+	public float[] diffuse;
+	public float[] specular;
+	public String texture;
+	public float shininess;
+	public float shinStren;
+	public float transparency;
+	public float selfIllum;
+	public int type;
+}
